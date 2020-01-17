17ba6200f53639b9d6c5edb4d62a66a0ecd17911
Pankaj <pgrover2079@gmail.com>
2020-01-16 17:32:47 -0800

    Serving Client from express server

--- challenge_4/server/index.js
+++ challenge_4/server/index.js
@@ -1,14 +1,37 @@
-// Import the express framework for our node server
+// // Import the express framework for our node server
+// const express = require('express');
+// // Import the path module from node to create absolute file paths for express static
+// const path = require('path');
+
+// // Instantiate the express server
+// const app = express();
+// // Set a constant for the port that our express server will listen on
+// const PORT = 3000;
+
+// // Serve static files. Any requests for specific files will be served if they exist in the provided folder
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+// // Start the server on the provided port
+// app.listen(PORT, () => console.log('Listening on port: ' + PORT));
+
+
 const express = require('express');
-// Import the path module from node to create absolute file paths for express static
+const morgan = require('morgan');
+const parser = require('body-parser');
 const path = require('path');
 
-// Instantiate the express server
 const app = express();
-// Set a constant for the port that our express server will listen on
+
 const PORT = 3000;
 
-// Serve static files. Any requests for specific files will be served if they exist in the provided folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
-// Start the server on the provided port
-app.listen(PORT, () => console.log('Listening on port: ' + PORT));
+app.use(morgan('dev'));
+app.use(parser.json());
+
+const clientPath = path.join(__dirname, '../client/dist');
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from server');
+})
+
+app.use(express.static(clientPath));
+
+app.listen(PORT, () => console.log(`Server listening to PORT ${PORT}`))
